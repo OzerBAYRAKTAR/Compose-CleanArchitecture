8abfe4e0094d6f955b9a4b6b4a9d47f5162522b0
Özer BAYRAKTAR <ozer.bayraktar6117@gmail.com>
2024-03-13 21:08:37 +0300

    first commit

---
+++ app/src/main/java/com/bayraktar/fakestoreapp/presentation/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.bayraktar.fakestoreapp.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.bayraktar.fakestoreapp.presentation.ui.theme.FakeStoreAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FakeStoreAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FakeStoreAppTheme {
+        Greeting("Android")
+    }
+}
