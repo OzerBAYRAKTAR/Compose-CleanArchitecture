f1117a772cce2de385052e186567781eaae4cc1c
Özer BAYRAKTAR <ozer.bayraktar6117@gmail.com>
2024-03-24 21:29:01 +0300

    cart use completed

--- app/src/main/java/com/bayraktar/fakestoreapp/domain/usecase/CartUseCase.kt
+++ app/src/main/java/com/bayraktar/fakestoreapp/domain/usecase/CartUseCase.kt
@@ -12,8 +12,82 @@ import javax.inject.Inject
 class CartUseCase @Inject constructor(
     private val repo: CartRepositoryImp
 ) {
-
     suspend fun getAllCarts() = flow {
+        try {
+            emit(APIResponse.Loading<BaseResponse<Users>>())
+            val users = repo.getAllCarts()
+            emit(APIResponse.Success(users))
+        } catch (e: HttpException){
+            emit(APIResponse.Error(
+                e.localizedMessage ?: "Beklenmeyen bir hata oluştu"
+            ))
+        }catch (e: IOException){
+            emit(APIResponse.Error(
+                e.localizedMessage ?: "Sunucuya ulaşılamıyor. İnternet bağlantınızı kontrol edin"
+            ))
+        }
+    }
+    suspend fun getSingleCart() = flow {
+        try {
+            emit(APIResponse.Loading<BaseResponse<Users>>())
+            val users = repo.getAllCarts()
+            emit(APIResponse.Success(users))
+        } catch (e: HttpException){
+            emit(APIResponse.Error(
+                e.localizedMessage ?: "Beklenmeyen bir hata oluştu"
+            ))
+        }catch (e: IOException){
+            emit(APIResponse.Error(
+                e.localizedMessage ?: "Sunucuya ulaşılamıyor. İnternet bağlantınızı kontrol edin"
+            ))
+        }
+    }
+    suspend fun getCartsByLimit() = flow {
+        try {
+            emit(APIResponse.Loading<BaseResponse<Users>>())
+            val users = repo.getAllCarts()
+            emit(APIResponse.Success(users))
+        } catch (e: HttpException){
+            emit(APIResponse.Error(
+                e.localizedMessage ?: "Beklenmeyen bir hata oluştu"
+            ))
+        }catch (e: IOException){
+            emit(APIResponse.Error(
+                e.localizedMessage ?: "Sunucuya ulaşılamıyor. İnternet bağlantınızı kontrol edin"
+            ))
+        }
+    }
+    suspend fun getUserCarts() = flow {
+        try {
+            emit(APIResponse.Loading<BaseResponse<Users>>())
+            val users = repo.getAllCarts()
+            emit(APIResponse.Success(users))
+        } catch (e: HttpException){
+            emit(APIResponse.Error(
+                e.localizedMessage ?: "Beklenmeyen bir hata oluştu"
+            ))
+        }catch (e: IOException){
+            emit(APIResponse.Error(
+                e.localizedMessage ?: "Sunucuya ulaşılamıyor. İnternet bağlantınızı kontrol edin"
+            ))
+        }
+    }
+    suspend fun createProduct() = flow {
+        try {
+            emit(APIResponse.Loading<BaseResponse<Users>>())
+            val users = repo.getAllCarts()
+            emit(APIResponse.Success(users))
+        } catch (e: HttpException){
+            emit(APIResponse.Error(
+                e.localizedMessage ?: "Beklenmeyen bir hata oluştu"
+            ))
+        }catch (e: IOException){
+            emit(APIResponse.Error(
+                e.localizedMessage ?: "Sunucuya ulaşılamıyor. İnternet bağlantınızı kontrol edin"
+            ))
+        }
+    }
+    suspend fun deletecart() = flow {
         try {
             emit(APIResponse.Loading<BaseResponse<Users>>())
             val users = repo.getAllCarts()
